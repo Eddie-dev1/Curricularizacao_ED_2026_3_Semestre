9e6d2e092f4bead336b14098d0c50165557e6d36
Eddie-dev1 <eduardoficial18@gmail.com>
2026-06-17 21:41:32 -0300

    Clean up Principal.java by removing author comments
    Removed comment block with author names.

--- Principal.java
+++ Principal.java
@@ -1,7 +1,3 @@
-/*EDUARDO ALVES DOS SANTOS
-FERNANDO FREIRE DE OLIVEIRA
-NICOLAS YUJI HIRATANI
-VICTOR SOARES RIELO*/
 public class Principal {
     public static void main(String[] args) {
         new Login_usuario().exibir(); 
